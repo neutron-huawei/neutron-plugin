902968d1c59ce45a024c0a96e347a608d1ecb1de
neutron-huawei <mayu0101@huawei.com>
2013-12-26 17:31:19 +0800

    modify nos_host nos_port in config.py

--- ml2_huawei_driver/huawei/config.py
+++ ml2_huawei_driver/huawei/config.py
@@ -27,44 +27,33 @@ from oslo.config import cfg
 
 
 Huawei_DRIVER_OPTS = [
-    cfg.StrOpt('eapi_username',
+    cfg.StrOpt('nos_username',
                default='',
                help=_('Username for Huawei sdn controller. This is required field.'
                       'if not set, all communications to Huawei sdn controller'
                       'will fail')),
-    cfg.StrOpt('eapi_password',
+    cfg.StrOpt('nos_password',
                default='',
                secret=True,  # do not expose value in the logs
                help=_('Password for Huawei sdn controller. This is required field.'
                       'if not set, all communications to Huawei sdn controller'
                       'will fail')),
-    cfg.StrOpt('eapi_host',
+    cfg.StrOpt('nos_host',
                default='',
                help=_('Huawei sdn controller IP address. This is required field.'
                       'If not set, all communications to Huawei sdn controller'
                       'will fail')),
-    cfg.BoolOpt('use_fqdn',
-                default=True,
-                help=_('Defines if hostnames are sent to Huawei sdn controller as FQDNs'
-                       '("node1.domain.com") or as short names ("node1").'
-                       'This is optional. If not set, a value of "True"'
-                       'is assumed.')),
+    cfg.IntOpt('nos_port',
+               default='',
+               help=_('Huawei sdn controller service port. This is required field.'
+                      'If not set, all communications to Huawei sdn controller'
+                      'will fail')),
     cfg.IntOpt('sync_interval',
                default=180,
                help=_('Sync interval in seconds between Neutron plugin and'
                       'sdn controller. This interval defines how often the'
                       'synchronization is performed. This is an optional'
-                      'field. If not set, a value of 180 seconds is assumed')),
-    cfg.StrOpt('region_name',
-               default='RegionOne',
-               help=_('Defines Region Name that is assigned to this OpenStack'
-                      'Controller. This is useful when multiple'
-                      'OpenStack/Neutron controllers are managing the same'
-                      'Huawei HW clusters. Note that this name must match with'
-                      'the region name registered (or known) to keystone'
-                      'service. Authentication with Keysotne is performed by'
-                      'sdn controller. This is optional. If not set, a value of'
-                      '"RegionOne" is assumed'))
+                      'field. If not set, a value of 180 seconds is assumed'))
 ]
 
 cfg.CONF.register_opts(Huawei_DRIVER_OPTS, "ml2_Huawei")
